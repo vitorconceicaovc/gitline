4ba84c9b4b92b607d4cf6b54944e1a9e6f417710
Vítor Conceição <vitoralmeidavc@outlook.pt>
2022-06-22 19:54:59 +0100

    animation configured

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,7 +7,7 @@ export const Nav = () => {
           <div className="links">
             <Link className="home" to="/">Home</Link>
             <a className='home' href="https://www.vitorconceicao.com/" target={'_blank'} >API</a>
-            <a className='home' href="https://www.vitorconceicao.com/" target={'_blank'} >Github</a>
+            <a className='home' href="https://github.com/vitorconceicaovc/gitline" target={'_blank'} >Github</a>
             <Link className="home" to="/about">About</Link>
             <Link className="demo" to="/demo">Demo</Link>
           </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 import './Home.css';
+import { Footer } from '../../components/Footer/Footer';
+import  Gitlogo  from '../../Assets/git_logo.png';
+import Wave from '../../Assets/wave.svg';
 
 export const Home = () => {
     return (
         <section className='home' >
+            <Footer/>
             <div className="info">
                 <h1 className='title'>GitLine</h1>
                 <h1 className='subtitle'>A melhor app de todas</h1>
@@ -14,8 +18,16 @@ export const Home = () => {
                 <Link className="demo" to="/demo">Get started</Link>
             </div>
             <div className="image">
-
+                <div className="icons">
+                    <img src={Gitlogo} alt="" />
+                    <img src={Gitlogo} alt="" />
+                    <img src={Gitlogo} alt="" />
+                    <img src={Gitlogo} alt="" />
+                    <img src={Gitlogo} alt="" />
+                </div>
+                <img className='gitlogo' src={Gitlogo} alt="" />
             </div>
+            
         </section>
     );
 }
