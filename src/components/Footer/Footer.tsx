20821c04b48bd4a247b66dee7747e5eb72e12af2
Vítor Conceição <vitoralmeidavc@outlook.pt>
2022-06-23 00:38:01 +0100

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,10 @@ import './Footer.css'
 
 export const Footer = () => {
     return(
-        <></>
+        <>
+            <footer>
+                <a href="https://www.vitorconceicao.com/" target={'_blank'} >© Vítor Conceição All rights reserved</a> 
+            </footer>
+        </>
     );
 }
