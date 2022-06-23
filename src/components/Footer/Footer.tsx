cb6dc7310baf48106950f1b1ef6c8a9699a45ea2
Vítor Conceição <vitoralmeidavc@outlook.pt>
2022-06-23 13:57:30 +0100

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,8 @@ export const Footer = () => {
     return(
         <>
             <footer>
-                <a href="https://www.vitorconceicao.com/" target={'_blank'} >© Vítor Conceição All rights reserved</a> 
+                <a  className='mobile' href="https://www.vitorconceicao.com/" target={'_blank'} >© Vítor Conceição</a> 
+                <a  className='pc' href="https://www.vitorconceicao.com/" target={'_blank'} >© Vítor Conceição All rights reserved</a>
             </footer>
         </>
     );
