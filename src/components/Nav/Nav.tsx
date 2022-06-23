f355e547128269961fa6427250fa977e3d0bd793
Vítor Conceição <vitoralmeidavc@outlook.pt>
2022-06-23 14:40:48 +0100

    added burguer

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom';
 import './Nav.css';
+import { HiMenu } from 'react-icons/hi';
 
 export const Nav = () => {
     return (
         <nav className='nav' >
           <div className="links">
-            <Link className="home" to="/">Home</Link>
-            <a className='home' href="https://github.com/octokit/rest.js/" target={'_blank'} >API</a>
-            <a className='home' href="https://github.com/vitorconceicaovc/gitline" target={'_blank'} >Github</a>
-            <Link className="home" to="/about">About</Link>
-            <Link className="demo" to="/demo">Demo</Link>
+            <Link className="home" id='pc' to="/">Home</Link>
+            <a className='home' id='pc' href="https://github.com/octokit/rest.js/" target={'_blank'} >API</a>
+            <a className='home' id='pc' href="https://github.com/vitorconceicaovc/gitline" target={'_blank'} >Github</a>
+            <Link className="home" id='pc' to="/about">About</Link>
+            <Link className="demo" id='pc' to="/demo">Demo</Link>
+            <button id='burguer'><HiMenu/></button>
           </div>
         </nav>
     );
