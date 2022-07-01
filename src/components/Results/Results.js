06572a818b01dab34f7b77e842cf41124ba82dd1
Vítor Conceição <vitoralmeidavc@outlook.pt>
2022-07-01 23:17:33 +0100

    demo style

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -5,17 +5,24 @@ export const Results = (props) => {
     const { repos } = props
     console.log("Repos:", repos.data)
 
+    
+        
+    
+    
     const listRepos = 
             repos.length !== 0 ? (
                 repos.data.map((item) => (
+
+                    
                     <li key={item.id} >
+                        
                         <div className='item'>
                             <div className='top' >
                                 <div className='title'><a className="" href={item.html_url} target='_blank' >{item.name}</a></div>
                                 <div className='date'><p>{item.created_at.slice(0, 10)}</p></div>
                             </div>
                             <div className='under' >
-                                <p>{item.description}</p>
+                                <p>{item.language}</p>
                             </div>
                             
                             
